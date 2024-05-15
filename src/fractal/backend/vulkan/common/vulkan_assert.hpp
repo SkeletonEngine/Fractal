c3d169049dc7fc5700b74e22aea480bf30652750
Jack <skeleton.engine@gmail.com>
2024-05-15 18:56:24 +0100

    Fix macros

--- src/fractal/backend/vulkan/common/vulkan_assert.hpp
+++ src/fractal/backend/vulkan/common/vulkan_assert.hpp
@@ -7,7 +7,8 @@
   #include <vulkan/vk_enum_string_helper.h>
   #include "fractal/base/common/assert.hpp"
 
-  #define FL_CONCAT(x, y) x##y
+  #define FL_CONCAT_INTERNAL(x, y) x##y
+  #define FL_CONCAT(x, y) FL_CONCAT_INTERNAL(x, y)
 
   #define VK_ASSERT(x) { VkResult FL_CONCAT(result, __LINE__) = x; if (FL_CONCAT(result, __LINE__) != VK_SUCCESS) { FL_LOG_ERROR("VK_ASSERT failed: {}", #x); FL_DEBUGBREAK(); } }
   #define VK_CHECK(x)  { VkResult FL_CONCAT(result, __LINE__) = x; if (FL_CONCAT(result, __LINE__) != VK_SUCCESS) { FL_LOG_ERROR("VK_CHECK failed: {}", #x);  FL_DEBUGBREAK(); } }
